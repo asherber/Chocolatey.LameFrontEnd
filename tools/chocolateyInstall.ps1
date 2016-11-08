8814a4401ee076d3715cd519c90ca1c279a3c8e8
Aaron Sherber <aaron@sherber.com>
2016-11-07 21:02:00 -0500

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageArgs = @{
+   packageName = 'lame-front-end'
+   fileType = 'exe'
+   silentArgs = '/verysilent'
+   url = 'http://www.pazera-software.com/files/Lame_Front-End.exe'
+   checksum = '138c34aebdfa2efd08163bb1327fe696150054670e81fd6e068384f7962cdc9d'
+   checksumType = 'sha256'
+}
+
+ 
+Install-ChocolateyPackage @packageArgs
